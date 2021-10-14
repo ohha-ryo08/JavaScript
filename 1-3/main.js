168c62dcb74f1f54503e135da093161cce753e7d
ohha-ryo08 <81632828+ohha-ryo08@users.noreply.github.com>
2021-10-14 22:53:16 +0900

    Initial commit

---
+++ 1-3/main.js
@@ -0,0 +1,31 @@
+//問１
+let a = 5;
+let b = 14;
+
+if(a != 10) {
+    console.log(a);
+}
+
+if(b >= 10 && b < 20){
+    console.log(b);
+}
+
+//問２
+let x = 10;
+
+if(x >= 10 && x <= 20){
+    console.log('成功です');
+}
+else{
+    console.log('失敗です');
+}
+
+//問３
+let y = 0;
+
+if(y % 2 === 0){
+    console.log('偶数です');
+}
+else{
+    console.log('奇数です');
+}
